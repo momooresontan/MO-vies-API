33c01a4ff895a6d84d6e3771152e10936c2671a0
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-06-26 09:30:16 +0100

    user model modified

--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,25 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Please enter a user name"],
+    unique: [true, "Username already taken"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter user email address"],
+    unique: [true, "Email address already taken."],
+    lowercase: true,
+    validate: [validator.isEmail, "User email invalid"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter user password"],
+  },
+  confirmPassword: {
+    type: String,
+    required: [true, "Please enter user password"],
+  },
+});
