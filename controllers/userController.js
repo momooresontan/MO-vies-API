aa289479f263fc6c1bc6cd1b166b5ae2fe19b1a0
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-06-26 09:52:52 +0100

    user controller modified

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,31 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
 const User = require("../models/userModel");
 
 exports.registerUser = asyncHandler(async (req, res) => {
   const { username, email, password, passwordConfirm } = req.body;
-  if (!username || !email || password || passwordConfirm)
-    res.status(201).json({ message: "Register user" });
+  if (!username || !email || !password || !passwordConfirm) {
+    res.status(400);
+    throw new Error("All fields are mandatory");
+  }
+  const userAvailable = await User.findOne({ email });
+  if (userAvailable) {
+    res.status(400);
+    throw new Error("User already created");
+  }
+
+  //Hash password
+  const hashedPassword = await bcrypt.hash(password, 10);
+  console.log(hashedPassword);
+  passwordConfirm = undefined;
+  const user = await User.create({
+    username,
+    email,
+    password: hashedPassword,
+    passwordConfirm: undefinded,
+  });
+
+  res.status(201).json({ message: "Register user" });
 });
 
 exports.loginUser = asyncHandler(async (req, res) => {
